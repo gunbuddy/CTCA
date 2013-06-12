10dd26caf4e84ca025eab6385deb330069889f30
Carlos Fernández <fernandez14@outlook.com>
2013-06-12 13:08:01 -0500

    First Cache Control for some queries

--- app/controllers/ProductsController.php
+++ app/controllers/ProductsController.php
@@ -2,13 +2,24 @@
 
 class ProductsController extends BaseController {
 	
-	public function __construct()
+	public function __construct(SubcategoriesRepository $subcategories)
 	{
+		$this->subcategories = $subcategories;
 	}
-
+	
 	public function showProducts($category)
 	{
-		$aller = App::make('Aller\Product\\' . ucfirst($category) .'\\' . ucfirst($category) . 'Interface');
+		// Check the category before checking products slug
+		$category_treatment = strtolower($category);
+		$get_subcategory    = $this->subcategories->getByAller($category_treatment);
+
+		if (!$get_subcategory)
+		{
+			// Subcategory with that aller not found
+			App::abort(404, 'Page not found');
+		}
+
+		$aller = App::make('Aller\Product\\' . ucfirst($category_treatment) .'\\' . ucfirst($category_treatment) . 'Interface');
 
 		if ($aller)
 		{
@@ -17,4 +28,6 @@ class ProductsController extends BaseController {
 			return $products;
 		}
 	}
+
+
 }
--- app/models/PostgresSubcategoriesRepository.php
+++ app/models/PostgresSubcategoriesRepository.php
@@ -4,6 +4,6 @@ class PostgresSubcategoriesRepository implements SubcategoriesRepository {
 	
 	public function getByAller($aller)
 	{
-		return Subcategory::where('aller', $aller)->first();
+		return Subcategory::where('aller', $aller)->remember(60)->first();
 	}
 }
